fcc637c4d6f7a6addfcbee892e65cb29026d684d
Bekzod Allaev <allaev_@outlook.com>
2023-10-15 22:42:13 +0500

    Add fluxor effect for weather and use it

---
+++ BlazorFluxor/Client/Store/Weather/WeatherEffects.cs
@@ -0,0 +1,25 @@
+﻿using BlazorFluxor.Shared;
+using Fluxor;
+using System.Net.Http.Json;
+
+namespace BlazorFluxor.Client.Store.Weather
+{
+    public class WeatherEffects
+    {
+        private readonly HttpClient Http;
+
+        public WeatherEffects(HttpClient http)
+        {
+            Http = http;
+        }
+
+        [EffectMethod(typeof(WeatherLoadForecastsAction))]
+        public async Task LoadForecasts(IDispatcher dispatcher)
+        {
+            dispatcher.Dispatch(new WeatherSetLoadingAction(true));
+            var forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast");
+            dispatcher.Dispatch(new WeatherSetForecastsAction(forecasts));
+            dispatcher.Dispatch(new WeatherSetLoadingAction(false));
+        }
+    }
+}
