f00568585aa628786d9df2b684cc8819f5029170
Bekzod Allaev <allaev_@outlook.com>
2023-10-17 22:21:44 +0500

    Add UserFeedbackState

---
+++ BlazorFluxor/Client/Store/Feedback/UserFeedbackState.cs
@@ -0,0 +1,12 @@
+﻿using BlazorFluxor.Shared;
+
+namespace BlazorFluxor.Client.Store.Feedback
+{
+    public record UserFeedbackState
+    {
+        public bool Submitting { get; init; }
+        public bool Submitted { get; init; }
+        public string ErrorMessage { get; init; }
+        public UserFeedbackModel Model { get; init; }
+    }
+}
