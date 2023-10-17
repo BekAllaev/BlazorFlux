6501ccac5b81804bdced1f39213b9aeb50a1c4de
Bekzod Allaev <allaev_@outlook.com>
2023-10-17 22:21:55 +0500

    Add UserFeedbackFeature

---
+++ BlazorFluxor/Client/Store/Feedback/UserFeedbackFeature.cs
@@ -0,0 +1,21 @@
+﻿using BlazorFluxor.Shared;
+using Fluxor;
+
+namespace BlazorFluxor.Client.Store.Feedback
+{
+    public class UserFeedbackFeature : Feature<UserFeedbackState>
+    {
+        public override string GetName() => "UserFeedback";
+
+        protected override UserFeedbackState GetInitialState()
+        {
+            return new UserFeedbackState
+            {
+                Submitting = false,
+                Submitted = false,
+                ErrorMessage = string.Empty,
+                Model = new UserFeedbackModel()
+            };
+        }
+    }
+}
