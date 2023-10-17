e30db92d8d498eac85b470045e2c82ae674f23f3
Bekzod Allaev <allaev_@outlook.com>
2023-10-17 22:29:06 +0500

    Add UserFeedbackEffects

---
+++ BlazorFluxor/Client/Store/Feedback/UserFeedbackEffects.cs
@@ -0,0 +1,29 @@
+﻿using Fluxor;
+using System.Net.Http.Json;
+
+namespace BlazorFluxor.Client.Store.Feedback
+{
+    public class UserFeedbackEffects
+    {
+        private readonly HttpClient _httpClient;
+        public UserFeedbackEffects(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [EffectMethod]
+        public async Task SubmitUserFeedback(UserFeedbackSubmitAction action, IDispatcher dispatcher)
+        {
+            var response = await _httpClient.PostAsJsonAsync("Feedback", action.UserFeedbackModel);
+
+            if (response.IsSuccessStatusCode)
+            {
+                dispatcher.Dispatch(new UserFeedbackSubmitSuccessAction());
+            }
+            else
+            {
+                dispatcher.Dispatch(new UserFeedbackSubmitFailureAction(response.ReasonPhrase));
+            }
+        }
+    }
+}
