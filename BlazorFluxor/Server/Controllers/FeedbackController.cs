19aaaf8820432941eb44bfdba7111ebc9027898b
Bekzod Allaev <allaev_@outlook.com>
2023-10-17 22:39:44 +0500

    Add FeedbackController and stub for the Post method

---
+++ BlazorFluxor/Server/Controllers/FeedbackController.cs
@@ -0,0 +1,21 @@
+﻿using BlazorFluxor.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorFluxor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedbackController : ControllerBase
+    {
+        [HttpPost]
+        public void Post(UserFeedbackModel model)
+        {
+            var email = model.EmailAddress;
+            var rating = model.Rating;
+            var comment = model.Comment;
+
+            Console.WriteLine($"Received rating {rating} from {email} with comment '{comment}'");
+        }
+    }
+}
