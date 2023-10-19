e23ae93050cca2dfc172ea5347c9214c5327d200
Bekzod Allaev <allaev_@outlook.com>
2023-10-19 20:31:34 +0500

    Add CounterPersistStateFailureAction

---
+++ BlazorFluxor/Client/Store/Counter/CounterPersistStateFailureAction.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorFluxor.Client.Store.Counter
+{
+    public class CounterPersistStateFailureAction
+    {
+        public string ErrorMessage { get; }
+        public CounterPersistStateFailureAction(string errorMessage)
+        {
+            ErrorMessage = errorMessage;
+        }
+    }
+}
