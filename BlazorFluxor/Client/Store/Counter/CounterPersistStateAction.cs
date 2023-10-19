e0e1162593fcbdba4fb66951a2ee273093f7e9a9
Bekzod Allaev <allaev_@outlook.com>
2023-10-19 20:30:08 +0500

    Add CounterPersistStateAction

---
+++ BlazorFluxor/Client/Store/Counter/CounterPersistStateAction.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorFluxor.Client.Store.Counter
+{
+    public class CounterPersistStateAction
+    {
+        public CounterState CounterState { get; }
+        public CounterPersistStateAction(CounterState counterState)
+        {
+            CounterState = counterState;
+        }
+    }
+}
