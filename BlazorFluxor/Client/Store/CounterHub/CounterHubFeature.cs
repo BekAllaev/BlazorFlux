1e23542626feede4e4ed0776f92dd2314d71e59b
Bekzod Allaev <allaev_@outlook.com>
2023-10-27 21:11:06 +0500

    Add CounterHubFeature

---
+++ BlazorFluxor/Client/Store/CounterHub/CounterHubFeature.cs
@@ -0,0 +1,17 @@
+﻿using Fluxor;
+
+namespace BlazorFluxor.Client.Store.CounterHub
+{
+    public class CounterHubFeature : Feature<CounterHubState>
+    {
+        public override string GetName() => "CounterHub";
+
+        protected override CounterHubState GetInitialState()
+        {
+            return new CounterHubState
+            {
+                Connected = false
+            };
+        }
+    }
+}
