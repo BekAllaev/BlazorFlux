f966101f0c3c004df5c46cd4be16ba0a4195d6f3
Bekzod Allaev <allaev_@outlook.com>
2023-10-27 21:56:17 +0500

    Add CounterHubReducer

---
+++ BlazorFluxor/Client/Store/CounterHub/CounterHubReducer.cs
@@ -0,0 +1,16 @@
+﻿using Fluxor;
+
+namespace BlazorFluxor.Client.Store.CounterHub
+{
+    public static class CounterHubReducers
+    {
+        [ReducerMethod]
+        public static CounterHubState OnSetConnected(CounterHubState state, CounterHubSetConnectedAction action)
+        {
+            return state with
+            {
+                Connected = action.connected
+            };
+        }
+    }
+}
